310b74ee5e86133886952107cc614439839395da
anst-foto <starininandrey@gmail.com>
2021-02-04 20:50:00 +0300

    Добавление структуры Address

---
+++ Address.h
@@ -0,0 +1,17 @@
+#ifndef UNTITLED_ADDRESS_H
+#define UNTITLED_ADDRESS_H
+
+#include <string>
+
+using namespace std;
+
+struct Address {
+    string country;
+    string region;
+    string city;
+    string street;
+    string house;
+    int post_code;
+};
+
+#endif //UNTITLED_ADDRESS_H
